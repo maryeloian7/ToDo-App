bca83bc63e481f8c984c5708ad5b894dc33bbe38
Maryia <maritsydik@gmail.com>
2023-05-02 14:05:56 +0200

    feat: working with components, adding a display

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
-import { addTodos, removeTodos, updateTodos } from "../redux/reducer";
+import {
+  addTodos,
+  completeTodos,
+  removeTodos,
+  updateTodos,
+} from "../redux/reducer";
 
 const mapStateProps = (state) => {
   return {
@@ -13,30 +18,17 @@ const mapDispatchToProps = (dispatch) => {
     addTodo: (obj) => dispatch(addTodos(obj)),
     removeTodo: (id) => dispatch(removeTodos(id)),
     updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (id) => dispatch(completeTodos(id)),
   };
 };
 
 const Todos = (props) => {
   const [todo, setTodo] = useState("");
 
-  const inputRef = useRef(true);
-
-  const changeFocus = () => {
-    inputRef.current.disabled = false;
-    inputRef.current.focus();
-  };
-
-  const update = (id, value, e) => {
-    if (e.which === 13) {
-      props.updateTodo({ id, item: value });
-      inputRef.current.disabled = true;
-    }
-  };
-
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
-  console.log("props from store", props);
+  // console.log("props from store", props);
   return (
     <div className="addTodos">
       <input
@@ -57,25 +49,6 @@ const Todos = (props) => {
         Add
       </button>
       <br />
-
-      <ul>
-        {props.todos.map((item) => {
-          return (
-            <li key={item.id}>
-              <textarea
-                ref={inputRef}
-                disabled={inputRef}
-                defaultValue={item.item}
-                onKeyDown={(e) => update(item.id, inputRef.current.value, e)}
-              />
-              <button onClick={() => changeFocus()}>Edit</button>
-              <button onClick={() => props.removeTodo(item.id)}>
-                Delete
-              </button>{" "}
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 };
